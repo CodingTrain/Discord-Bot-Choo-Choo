fb439c7c6bf26d7202ea6ad8b9e526413284e419
Daniel Shiffman <daniel@shiffman.net>
2020-11-09 08:52:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,35 @@ require('dotenv').config();
 const serverID = process.env.SERVERID;
 const channelID = process.env.CHANNELID;
 
-client.once('ready', () => {
-  console.log('Ready!');
-});
+console.log('Beep beep! 🤖');
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+// I recommend using dotenv, you can see how the project is setup here: 
+// https://github.com/CodingTrain/Discord-Bot-Choo-Choo
+// dotenv will be covered in a future video
 client.login(process.env.TOKEN);
+
+client.on('ready', readyDiscord);
+
+function readyDiscord() {
+  console.log('💖');
+}
+
+const replies = [
+  '🚂🌈💖',
+  'Choo choo!',
+  'Ding! 🛎',
+  'Never forget this dot!'
+]
+
 client.on('message', gotMessage);
 
 function gotMessage(msg) {
-  console.log(msg);
-  // Only for this server and this channel
   if (msg.guild.id === serverID && msg.channel.id === channelID) {
-    if (msg.content === 'ping') {
-      msg.channel.send('pong');
+    if (msg.content === '!choochoo') {
+      const index = Math.floor(Math.random() * replies.length);
+      msg.channel.send(replies[index]);
     }
   }
 }
